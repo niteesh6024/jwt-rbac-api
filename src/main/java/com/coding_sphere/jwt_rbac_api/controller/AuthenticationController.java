b05fb793c1739c96d5b48f75bf2d3cbdaee9dc11
niteesh-sk <boddapuniteesh541@gmail.com>
2024-12-06 15:30:28 +0530

    outline basic structure

---
+++ src/main/java/com/coding_sphere/jwt_rbac_api/controller/AuthenticationController.java
@@ -0,0 +1,4 @@
+package com.coding_sphere.jwt_rbac_api.controller;
+
+public class AuthenticationController {
+}
--- src/main/java/com/coding_sphere/jwt_rbac_api/controller/ProjectController.java
+++ src/main/java/com/coding_sphere/jwt_rbac_api/controller/ProjectController.java
@@ -1,5 +1,33 @@
 package com.coding_sphere.jwt_rbac_api.controller;
 
+import com.coding_sphere.jwt_rbac_api.entity.Project;
+import com.coding_sphere.jwt_rbac_api.entity.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api")
 public class ProjectController {
+
+    private List<User> user =  new ArrayList<>();
+    private List<Project> projects =  new ArrayList<>();
+
+    @GetMapping("/projects")
+    private ResponseEntity<?> getprojects(){
+        return new ResponseEntity<String>("succc",null, HttpStatus.OK);
+    }
+
+    @PostMapping("/projects")
+    private ResponseEntity<?> createProjects(@RequestBody Project project){
+        project.setId(UUID.randomUUID().toString());
+        project.setUser(null);
+        projects.add(project);
+        return new ResponseEntity<List<Project>>(projects,null, HttpStatus.OK);
+    }
+
 }
